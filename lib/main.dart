ed1df3dd46d4f3a89b74fcdb712342bc0566b6ee
mehmetefeozcan <ozcanmehmetefe@gmail.com>
2023-03-18 19:57:15 +0300

    add marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:huawei_map/map.dart';
 
@@ -30,34 +31,58 @@ class _HuaweiMapDemoState extends State<HuaweiMapDemo> {
   static const LatLng _center = LatLng(41.012959, 28.997438);
   static const double _zoom = 12;
 
-  bool onLoading = false;
+  final Set<Marker> markers = <Marker>{};
+  int id = 0;
 
   @override
   void initState() {
     HuaweiMapInitializer.initializeMap();
-
     super.initState();
+  }
+
+  createMarker(double lat, double lng) {
+    setState(() {
+      final marker = Marker(
+        markerId: MarkerId("${id + 1}"),
+        position: LatLng(lat, lng),
+        infoWindow: const InfoWindow(title: "Antidepresan"),
+      );
+      markers.add(marker);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HuaweiMap(
-        initialCameraPosition: CameraPosition(target: _center, zoom: _zoom),
-        mapType: MapType.normal,
-        tiltGesturesEnabled: true,
-        buildingsEnabled: true,
-        compassEnabled: true,
-        zoomControlsEnabled: true,
-        rotateGesturesEnabled: true,
-        myLocationButtonEnabled: true,
-        myLocationEnabled: true,
-        padding: EdgeInsets.only(bottom: 30.0),
-        logoPosition: HuaweiMap.LOWER_LEFT,
-        logoPadding: EdgeInsets.only(
-          left: 15,
-          bottom: 75,
-        ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          HuaweiMap(
+            initialCameraPosition:
+                const CameraPosition(target: _center, zoom: _zoom),
+            mapType: MapType.normal,
+            markers: markers,
+            tiltGesturesEnabled: true,
+            buildingsEnabled: true,
+            compassEnabled: true,
+            zoomControlsEnabled: true,
+            rotateGesturesEnabled: true,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            padding: const EdgeInsets.only(bottom: 30.0),
+            logoPosition: HuaweiMap.LOWER_LEFT,
+            logoPadding: const EdgeInsets.only(
+              left: 15,
+              bottom: 75,
+            ),
+            onPoiClick: (argument) {},
+            onClick: (argument) {
+              setState(() {
+                createMarker(argument.lat, argument.lng);
+              });
+            },
+            onLongPress: (argument) => markers.clear(),
+          ),
+        ],
       ),
     );
   }
